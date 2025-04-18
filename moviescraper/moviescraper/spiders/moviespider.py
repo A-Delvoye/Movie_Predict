8c90ece7390c0ac70df8a12d34d024c49681eb8f
John Doe <samikabdani.pro@gmail.com>
2025-04-18 12:17:01 +0200

    add integral_scraping

--- moviescraper/moviescraper/spiders/moviespider.py
+++ moviescraper/moviescraper/spiders/moviespider.py
@@ -5,13 +5,10 @@ class MoviespiderSpider(scrapy.Spider):
     name = "moviespider"
     allowed_domains = ["www.allocine.fr"]
     start_urls = ["https://www.allocine.fr/films/decennie-2000/", "https://www.allocine.fr/films/decennie-2010/", "https://www.allocine.fr/films/decennie-2020/"]
-    # start_urls=['https://www.allocine.fr/films/alphabetique/decennie-2020/?page=46']
 
 
     def parse(self, response):
-        # for start_url in self.start_urls:
         number_of_pages = int(response.css('div.pagination-item-holder span.button-md.item::text').getall()[-1])
-            # for i in range(1, number_of_pages+1):
         for i in range(1, number_of_pages):
             next_page_url = response.url + f'?page={i}'
 
@@ -34,29 +31,13 @@ class MoviespiderSpider(scrapy.Spider):
                 categories_item+=categorie+','
             categories_item=categories_item[:-1]
             realisator = movie.css('div.meta-body-item.meta-body-direction span.dark-grey-link::text').getall()
-            actors=movie.css('div.meta-body-item.meta-body-actor span.dark-grey-link::text').getall()
-            actors_list = []
-            for actor in actors:
-                actors_list.append(actor)
-            match len(actors_list):
-                case 1:
-                    movieItem['actor1']=actors_list[0]
-                case 2:
-                    movieItem['actor1']=actors_list[0]
-                    movieItem['actor2']=actors_list[1]
-                case 3:
-                    movieItem['actor1']=actors_list[0]
-                    movieItem['actor2']=actors_list[1]
-                    movieItem['actor3']=actors_list[2]
-                case _:
-                    pass                
             
             movie_url = movie.css('a.meta-title-link').attrib['href']
             movie_number = ''.join([x for x in movie_url if x.isdigit()])
             movieItem['title'] = title
             movieItem['duration'] = duration
             movieItem['categories'] = categories_item
-            movieItem['realisator_de'] = realisator
+            movieItem['realisator'] = realisator
             movieItem['movie_number']=movie_number
             yield response.follow(
                 url='https://www.allocine.fr'+movie_url,
@@ -64,23 +45,6 @@ class MoviespiderSpider(scrapy.Spider):
                 meta={'item': movieItem}
             )
 
-
-
-
-    def parse_trailer(self, response):
-        movie = response.css('div.media-info-item.icon.icon-eye')
-        movieItem = response.meta['item']
-        if movieItem['sortie_france']:
-            trailer_views = movie.css('::text').get().strip()
-            movieItem["trailer_views"] = trailer_views
-            yield response.follow(
-                    url='https://www.allocine.fr/film/fichefilm-'+movieItem['movie_number']+'/box-office/',
-                    callback= self.parse_box_office,
-                    meta={'item': movieItem}
-                )
-        else:
-            pass
-        
 
     def parse_movie(self,response):
         movieItem = response.meta['item']
@@ -90,29 +54,76 @@ class MoviespiderSpider(scrapy.Spider):
         country = response.css('section.ovw-technical span.nationality ::text').getall()
         distributor = response.xpath('//div[contains(@class, "item")]/span[contains(text(), "Distributeur")]/following-sibling::span/text()').get()
         trailer_url = response.css('a.trailer.roller-item::attr(href)').get()
+        casting_url = response.xpath('//a[contains(@title, "Casting")]/@href').get()
         budget = response.xpath('//div[contains(@class, "item")]/span[contains(text(), "Budget")]/following-sibling::span/text()').get()
-        annee = response.xpath('//div[contains(@class, "item")]/span[contains(text(), "Année de production")]/following-sibling::span/text()').get()
+        year = response.xpath('//div[contains(@class, "item")]/span[contains(text(), "Année de production")]/following-sibling::span/text()').get()
         sortie_france = True if response.xpath('//div[contains(@class, "item")]/span[contains(text(), "Box Office France")]/following-sibling::span/text()').get()  else False
         critics_rating = response.css('div.rating-holder.rating-holder-3 span.stareval-note ::text').get()
         movieItem['release_date']= release_date
         movieItem['distributor']=distributor
         movieItem['awards']=awards
-        movieItem['production_year']=annee
+        movieItem['year']=year
         movieItem['budget'] = budget
         movieItem['country']= country
-        movieItem['scenario_par']=producer
+        movieItem['producer']=producer
         movieItem['critics_rating']=critics_rating
         movieItem['sortie_france']=sortie_france
-        if trailer_url:
+        movieItem['trailer_url']=trailer_url
+
+        movie_number = movieItem['movie_number']
+
+        if casting_url : 
             yield response.follow(
-                    url='https://www.allocine.fr'+trailer_url,
-                    callback= self.parse_trailer,
-                    meta={'item': movieItem}
+                url='https://www.allocine.fr/film/fichefilm-'+movie_number+'/casting/',
+                callback=self.parse_casting,
+                meta={'item': movieItem}
+            )
+        elif not casting_url and trailer_url:
+            yield response.follow(
+                url='https://www.allocine.fr'+trailer_url,
+                callback=self.parse_trailer,
+                meta={'item': movieItem}
                 )
-        else:
-            pass
+
+    def parse_casting(self, response):
+
+        movieItem = response.meta['item']
+        trailer_url = movieItem['trailer_url']
+
+        casting1 = response.xpath('//div[contains(@class, "card person-card person-card-col")]//div[@class="meta-title"]/a/text()').getall()
+        casting2 = response.css('section.casting-actor div.meta-title span.meta-title-link::text').getall() 
+        movieItem['casting'] = casting1 + casting2
+        movie_number = movieItem['movie_number']
+
+
+        if trailer_url : 
+            yield response.follow(
+                url='https://www.allocine.fr' + trailer_url,
+                callback=self.parse_trailer,
+                meta={'item': movieItem}
+                )
+        elif not trailer_url :
+            yield response.follow(
+                url='https://www.allocine.fr/film/fichefilm-'+movie_number+'/box-office/',
+                callback=self.parse_box_office,
+                meta={'item': movieItem}
+            )
+
+    def parse_trailer(self, response):
+
+        movie = response.css('div.media-info-item.icon.icon-eye')
+        movieItem = response.meta['item']
+        trailer_views = movie.css('::text').get()
+        movieItem["trailer_views"] = trailer_views.strip() if trailer_views else None
+
+        yield response.follow(
+                url='https://www.allocine.fr/film/fichefilm-'+movieItem['movie_number']+'/box-office/',
+                callback= self.parse_box_office,
+                meta={'item': movieItem}
+            )
 
     def parse_box_office(self, response):
+
         movieItem = response.meta['item']
         tables= response.css('section.section')
 
@@ -132,24 +143,5 @@ class MoviespiderSpider(scrapy.Spider):
                     else:
                         movieItem['US_first_week']= table_box_office[0].strip().replace(" ","")
 
-
-
-        # for index, table in enumerate(tables):
-        #     table_box_office = table.css('td.responsive-table-column.second-col.col-bg::text').getall()
-        #     if table_box_office:
-        #         if len(table_box_office)>1:
-        #             n_first_week = max(int(table_box_office[0].strip().replace(" ", "")), int(table_box_office[1].strip().replace(" ", "")))
-        #         else:
-        #             n_first_week= table_box_office[0].strip().replace(" ","")
-        #     else:
-        #         n_first_week = ""
-
-        #     if index == 1:
-        #         France_first_week = n_first_week
-        #     elif index ==2:
-        #         US_first_week = n_first_week
-                
-        # movieItem['France_first_week'] = France_first_week
-        # movieItem['US_first_week'] = US_first_week
 
         yield movieItem
