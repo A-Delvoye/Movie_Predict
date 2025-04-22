622435c424ec15b17dc45513dd4be08a2008a68c
Daviddavid-sudo <david.scott.2875@gmail.com>
2025-04-22 11:44:41 +0200

    added security to prediction

--- app/api/v1/endpoints/prediction.py
+++ app/api/v1/endpoints/prediction.py
@@ -6,14 +6,14 @@ from app.schemas.predict_functions import start_prediction
 from app.api.v1.endpoints.auth import get_current_user
 from app.db.session import get_session
 from sqlmodel import Session
+from app.models.user import User
 import numpy as np
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @router.post("/predict")
-#need to add security feature, anyone can make a prediction
-async def predict():
+async def predict(current_user: User = Depends(get_current_user)):
     pred_df = start_prediction()
     pred_df = pred_df.replace([np.nan, np.inf, -np.inf], None)
     return JSONResponse(content={"predictions": pred_df.to_dict(orient="records")})
